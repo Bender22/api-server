58deb7ae0178487b1aaf7da161deb469c1e6399a
Bender <rahiguera91@gmail.com>
2024-01-20 01:49:53 -0500

    Initial Commit

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,7 @@
 import '../../src/database/mongodb.js'
 import express from 'express'
-// import logger from 'morgan'
-// import helmet from 'helmet'
+import logger from 'morgan'
+import helmet from 'helmet'
 import eventRouter from '../../src/router/eventRouter.js'
 import serverless from 'serverless-http'
 // import errorsHandler from './middleware/errorsHandler.js'
@@ -10,12 +10,11 @@ import serverless from 'serverless-http'
 // }).finally()
 const app = express()
 
-// app.use(helmet())
-
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
+app.use(helmet())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/api/', eventRouter)
-// app.use('/api', playerRouter)
 // app.use(errorsHandler)
 
 export const handler = serverless(app)
