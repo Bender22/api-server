6cf13c5e3c4b2634cfd6143136cf6a8c668f5009
Bender <rahiguera91@gmail.com>
2024-01-20 21:28:54 -0500

    Initial Commit

---
+++ netlify/functions/api-docs.js
@@ -0,0 +1,22 @@
+import swaggerUI from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Undead-Api',
+      version: '1.0.0'
+    }
+  },
+  servers: [{ url: 'http://localhost:4000' }],
+  apis: [`${path.join(__dirname, '../../src/routes/*.js')}`]
+}
+
+const swaggerDocs = swaggerJsDoc(options)
+const app = express()
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
