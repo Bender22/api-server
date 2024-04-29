ad2bf94249f687bda553905b81fc28943c0977ad
Punky <rahiguera91@gmail.com>
2024-04-29 16:28:21 -0400

    adicionado cors

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -15,7 +15,9 @@ const app = express()
 app.use(cors({
   origin: '*'
 }))
-app.use(helmet())
+app.use(helmet({
+  crossOriginResourcePolicy: false
+}))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
