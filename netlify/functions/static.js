f80380fa082b7fa0b0a6b5b35d350c307f76f517
Punky <rahiguera91@gmail.com>
2024-04-29 18:17:12 -0400

    adicionado cors

--- netlify/functions/static.js
+++ netlify/functions/static.js
@@ -2,13 +2,14 @@ import '../../src/database/mongodb.js'
 import express from 'express'
 import serverless from 'serverless-http'
 import * as path from 'path'
-// import { fileURLToPath } from 'url'
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const app = express()
-app.use(express.static('src/public/'))
+console.log()
+app.use(express.static(__dirname))
 app.get('/', (req, res) => {
-  res.sendFile('/src/public/index.html')
+  res.sendFile(__dirname)
 })
 
 export const handler = serverless(app)
