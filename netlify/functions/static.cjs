d4e7e33af939f36694d289486428a43c5876c494
Punky <rahiguera91@gmail.com>
2024-04-29 18:32:10 -0400

    adicionado cors

--- netlify/functions/static.cjs
+++ netlify/functions/static.cjs
@@ -6,9 +6,9 @@ const path = require('path')
 // const __filename = fileURLToPath(import.meta.url)
 // const __dirname = path.dirname(__filename)
 const app = express()
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('.netlify/public/'))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile('.netlify/public/index.html')
 })
 
 export const handler = serverless(app)
